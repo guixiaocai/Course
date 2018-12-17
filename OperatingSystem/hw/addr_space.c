279d64db55b1f3e2bce806e02e3b6f6cc8a947d7
$NULL <37217083+kyrie2333@users.noreply.github.com>
2018-12-17 16:02:06 +0800

    Add files via upload

---
+++ OperatingSystem/hw/addr_space.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *myname="Chen Mingyu";
+char gdata[128];
+char bdata[16] = {1,2,3,4};
+main() {
+	char * ldata[16];	
+	char * ddata;
+
+	ddata = malloc(16);
+	printf("gdata: %llX\nbdata:%llX\nldata:%llx\nddata:%llx\n",
+		gdata,bdata,ldata,ddata);
+	free(ddata);
+}
+
+
+module my_cpu(
+    input [0:0] clk,
+    input [0:0] resetn,
+    output [0:0] inst_sram_en,
+    output [3:0] inst_sram_wen,
+    output [31:0] inst_sram_addr,
+    output [31:0] inst_sram_wdata,
+    input [31:0] inst_sram_rdata,
+    output [0:0] data_sram_en,
+    output [3:0] data_sram_wen,
+    output [31:0] data_sram_addr,
+    output [31:0] data_sram_wdata,
+    input [31:0] data_sram_rdata,
+    output [31:0] debug_wb_pc,
+    output [3:0] debug_wb_rf_wen,
+    output [4:0] debug_wb_rf_wnum,
+    output [31:0] debug_wb_rf_wdata
+    );
+    
+endmodule
